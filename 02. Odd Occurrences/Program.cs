22c65fa3f5277fd2583fcb6eebc41b9140d312bc
Boyan Venkov <boyanvenkov@yahoo.com>
2025-11-07 18:51:14 +0200

    Added a New Project

--- 02. Odd Occurrences/Program.cs
+++ 02. Odd Occurrences/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿string[] words = Console.ReadLine().Split();
+
+Dictionary<string, int> oddWord = new();
+
+foreach (string word in words)
+{
+    string wordWithLowerLetter = word.ToLower();
+    if (!oddWord.ContainsKey(wordWithLowerLetter))
+    {
+        oddWord.Add(wordWithLowerLetter, 1);
+    }
+    else
+    {
+        oddWord[wordWithLowerLetter]++;
+    }
+}
+foreach (KeyValuePair<string, int> pair in oddWord)
+{
+    if (pair.Value % 2 != 0)
+    {
+        Console.Write(pair.Key + " ");
+    }
+}
