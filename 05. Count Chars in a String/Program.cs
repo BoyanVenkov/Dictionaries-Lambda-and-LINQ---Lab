bc576ed720b6d2d2dde405005a675b69bf5c7806
Boyan Venkov <boyanvenkov@yahoo.com>
2025-11-07 18:31:40 +0200

    Added a New Project

---
+++ 05. Count Chars in a String/Program.cs
@@ -0,0 +1,25 @@
+﻿string text = Console.ReadLine();
+
+Dictionary<char, int> symbols = new Dictionary<char, int>();
+
+foreach (char symbol in text)
+{
+    if (symbol == ' ')
+    {
+        continue;
+    }
+
+    if (!symbols.ContainsKey(symbol))
+    {
+        symbols.Add(symbol, 1);
+    }
+    else
+    {
+        symbols[symbol]++;
+    }
+}
+foreach (KeyValuePair<char, int> pair in symbols)
+{
+    Console.WriteLine(pair.Key + " -> " + pair.Value);
+
+}
