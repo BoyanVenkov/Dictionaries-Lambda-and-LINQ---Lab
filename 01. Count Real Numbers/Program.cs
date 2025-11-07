11afd3b7f2284183e6204c8ff6ae38c302cbb8df
Boyan Venkov <boyanvenkov@yahoo.com>
2025-11-07 15:56:36 +0200

    Add project files.

---
+++ 01. Count Real Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+SortedDictionary<int, int> countOccurances = new();
+
+foreach (int num in numbers)
+{
+    if (countOccurances.ContainsKey(num))
+    {
+        countOccurances[num]++;
+    }
+    else
+    {
+        countOccurances.Add(num, 1);
+    }
+}
+foreach (KeyValuePair<int, int> pair in countOccurances)
+{
+    Console.WriteLine(pair.Key + " -> " + pair.Value);
+}
