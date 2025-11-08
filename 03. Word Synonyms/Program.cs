2cd03329d0f5ce72b9fa706d1dcffefd55387ef2
Boyan Venkov <boyanvenkov@yahoo.com>
2025-11-08 10:37:42 +0200

    Added a New Project

---
+++ 03. Word Synonyms/Program.cs
@@ -0,0 +1,23 @@
+﻿int countWords = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<string>> wordSynonims = new();
+
+for (int i = 1; i <= countWords; i++)
+{
+    string word = Console.ReadLine();
+    string synonim = Console.ReadLine();
+
+    if (!wordSynonims.ContainsKey(word))
+    {
+        wordSynonims.Add(word, new List<string> { synonim });
+    }
+    else
+    {
+        wordSynonims[word].Add(synonim);
+    }
+}
+foreach (KeyValuePair<string, List<string>> pair in wordSynonims)
+{
+    Console.WriteLine(pair.Key + " - " + string.Join(", ", pair.Value));
+
+}
