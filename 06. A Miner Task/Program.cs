79b1cd80191e8e7dcdd0fdafd5c3cac5e0215a67
Boyan Venkov <boyanvenkov@yahoo.com>
2025-11-08 10:46:19 +0200

    Added a New Project

---
+++ 06. A Miner Task/Program.cs
@@ -0,0 +1,21 @@
+﻿string resource = Console.ReadLine();
+Dictionary<string, int> resourceQuantity = new();
+
+while (resource != "stop")
+{ 
+    int quantity = int.Parse(Console.ReadLine());
+    if (!resourceQuantity.ContainsKey(resource))
+    {
+        resourceQuantity.Add(resource, quantity);
+    }
+    else
+    {
+        resourceQuantity[resource]+= quantity;
+    }
+
+        resource = Console.ReadLine();
+}
+foreach (KeyValuePair<string, int> pair in resourceQuantity)
+{
+    Console.WriteLine(pair.Key + " -> " + pair.Value);
+}
